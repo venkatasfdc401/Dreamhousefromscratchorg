3b2dfcf8f5b4b0804e645ef6c65e6024c8abfb92
Venkata Rajesh <rchidagani@salesforce.com>
2019-03-04 10:33:33 +0530

    first source commit message

---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,20 @@
+declare module "@salesforce/resourceUrl/botCommands" {
+    var botCommands: string;
+    export default botCommands;
+}
+declare module "@salesforce/resourceUrl/brokers" {
+    var brokers: string;
+    export default brokers;
+}
+declare module "@salesforce/resourceUrl/leaflet" {
+    var leaflet: string;
+    export default leaflet;
+}
+declare module "@salesforce/resourceUrl/nouislider" {
+    var nouislider: string;
+    export default nouislider;
+}
+declare module "@salesforce/resourceUrl/properties" {
+    var properties: string;
+    export default properties;
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Dreamhousefromscratchorg
+# SFDX  App
+
+## Dev, Build and Test
+
+
+## Resources
+
+
+## Description of Files and Directories
+
+
+## Issues
+
+
